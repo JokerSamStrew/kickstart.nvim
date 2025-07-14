5afd9538b3e57cf4d2782381f57b7311d719bd79
Simon Labzoff <semyon@flatinn.ru>
2025-07-14 09:30:42 +0300

    ✨ feat: gen ollama integration

--- init.lua
+++ init.lua
@@ -1096,6 +1096,7 @@ require('lazy').setup({
   plugins.venv_selector(),
   plugins.nvim_treesitter_context(),
   plugins.nvim_emmet(),
+  plugins.gen(),
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
--- plugins.lua
+++ plugins.lua
@@ -1,5 +1,153 @@
 local plugins = {}
 
+function plugins.gen()
+  return {
+    'David-Kunz/gen.nvim',
+    opts = {
+      -- model = 'qwen2.5-coder:0.5b', -- The default model to use.
+      model = 'codegemma:latest', -- The default model to use.
+
+      quit_map = 'q', -- set keymap to close the response window
+      retry_map = '<c-r>', -- set keymap to re-send the current prompt
+      accept_map = '<c-cr>', -- set keymap to replace the previous selection with the last result
+      host = 'localhost', -- The host running the Ollama service.
+      port = '11434', -- The port on which the Ollama service is listening.
+      display_mode = 'float', -- The display mode. Can be "float" or "split" or "horizontal-split" or "vertical-split".
+      show_prompt = false, -- Shows the prompt submitted to Ollama. Can be true (3 lines) or "full".
+      show_model = false, -- Displays which model you are using at the beginning of your chat session.
+      no_auto_close = false, -- Never closes the window automatically.
+      file = false, -- Write the payload to a temporary file to keep the command short.
+      hidden = false, -- Hide the generation window (if true, will implicitly set `prompt.replace = true`), requires Neovim >= 0.10
+      init = function(options)
+        pcall(io.popen, 'ollama serve > /dev/null 2>&1 &')
+      end,
+      -- Function to initialize Ollama
+      command = function(options)
+        local body = { model = options.model, stream = true }
+        return 'curl --silent --no-buffer -X POST http://' .. options.host .. ':' .. options.port .. '/api/chat -d $body'
+      end,
+      -- The command for the Ollama service. You can use placeholders $prompt, $model and $body (shellescaped).
+      -- This can also be a command string.
+      -- The executed command must return a JSON object with { response, context }
+      -- (context property is optional).
+      -- list_models = '<omitted lua function>', -- Retrieves a list of model names
+      result_filetype = 'markdown', -- Configure filetype of the result buffer
+      debug = false, -- Prints errors and the command which is run.
+    },
+  }
+end
+
+function plugins.avante()
+  return {
+    'yetone/avante.nvim',
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    -- ⚠️ must add this setting! ! !
+    build = function()
+      -- conditionally use the correct build system for the current OS
+      if vim.fn.has 'win32' == 1 then
+        return 'powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false'
+      else
+        return 'make'
+      end
+    end,
+    event = 'VeryLazy',
+    version = false, -- Never set this value to "*"! Never!
+    ---@module 'avante'
+    ---@type avante.Config
+    opts = {
+      -- add any opts here
+      -- for example
+      -- provider = 'claude',
+      -- providers = {
+      --   claude = {
+      --     endpoint = 'https://api.anthropic.com',
+      --     model = 'claude-sonnet-4-20250514',
+      --     timeout = 30000, -- Timeout in milliseconds
+      --     extra_request_body = {
+      --       temperature = 0.75,
+      --       max_tokens = 20480,
+      --     },
+      --   },
+      -- },
+      provider = 'ollama',
+      providers = {
+        ollama = {
+          endpoint = 'http://127.0.0.1:11434',
+          model = 'qwen2.5-coder:0.5b',
+          timeout = 30000,
+          -- disable_tools = true, -- disables all tools
+          disabled_tools = {
+            -- 'rag_search',
+            'python',
+            -- 'git_diff',
+            'git_commit',
+            -- 'list_files',
+            -- 'search_files',
+            -- 'search_keyword',
+            -- 'read_file_toplevel_symbols',
+            -- 'read_file',
+            'create_file',
+            'rename_file',
+            'delete_file',
+            'create_dir',
+            'rename_dir',
+            'delete_dir',
+            'bash',
+            -- 'web_search',
+            -- 'fetch',
+          },
+          extra_request_body = {
+            options = {
+              temperature = 0.75,
+              num_ctx = 2048,
+              max_completion_tokens = 512, -- limit output tokens here
+              keep_alive = '15m',
+            },
+          },
+        },
+      },
+    },
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'MunifTanjim/nui.nvim',
+      --- The below dependencies are optional,
+      'echasnovski/mini.pick', -- for file_selector provider mini.pick
+      'nvim-telescope/telescope.nvim', -- for file_selector provider telescope
+      'hrsh7th/nvim-cmp', -- autocompletion for avante commands and mentions
+      'ibhagwan/fzf-lua', -- for file_selector provider fzf
+      'stevearc/dressing.nvim', -- for input provider dressing
+      'folke/snacks.nvim', -- for input provider snacks
+      'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
+      'zbirenbaum/copilot.lua', -- for providers='copilot'
+      {
+        -- support for image pasting
+        'HakonHarnes/img-clip.nvim',
+        event = 'VeryLazy',
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { 'markdown', 'Avante' },
+        },
+        ft = { 'markdown', 'Avante' },
+      },
+    },
+  }
+end
+
 function plugins.leetcode()
   return {
     'kawre/leetcode.nvim',
@@ -10,7 +158,10 @@ function plugins.leetcode()
       'MunifTanjim/nui.nvim',
     },
     lazy = 'leetcode.nvim' ~= vim.fn.argv(0, -1),
-    opts = { arg = leet_arg },
+    opts = {
+      arg = 'leetcode.nvim',
+      lang = 'javascript',
+    },
   }
 end
 
